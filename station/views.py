ddd58ad8bd98318cc43cb42e93500fae3764eb60
Jonas Alder <jonas.alder@gmail.com>
2016-09-11 14:55:27 +0200

    - basic django rest_framework setup - added requires.txt

--- station/views.py
+++ station/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import serializers, viewsets
+
+
+def serve_main_page(request):
+    return render(request, "main.html")
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
