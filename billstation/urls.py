80718578c7a8c2bfe574a4085e0303dfd2780cc5
Jonas Alder <jonas.alder@gmail.com>
2016-09-25 03:35:58 +0200

    - added basic swagger stuff and generated client class

--- billstation/urls.py
+++ billstation/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from station.views import DudeViewSet, BillViewSet, PaymentViewSet, serve_main_page
+from station.views import DudeViewSet, BillViewSet, PaymentViewSet, serve_main_page, schema_view, schema_view_raw
 
 router = routers.DefaultRouter()
 router.register(r'dudes', DudeViewSet)
@@ -27,5 +27,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'api/', include(router.urls)),
+    url(r'swagger-schema/', schema_view),
+    url(r'swagger-schema-raw/', schema_view_raw),
     url(r'^$', serve_main_page, name='main'),
 ]
--- station/views.py
+++ station/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from rest_framework import response, schemas, serializers, viewsets
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
 from .models import *
 
-from rest_framework import serializers, viewsets
-
-
+# Serves and renders the AngularJS main page
 def serve_main_page(request):
     return render(request, "main.html")
 
+
+@api_view()
+@renderer_classes([OpenAPIRenderer, SwaggerUIRenderer])
+def schema_view(request):
+    generator = schemas.SchemaGenerator(title='Billstation API')
+    return response.Response(generator.get_schema(request=request))
+
+@api_view()
+@renderer_classes([OpenAPIRenderer])
+def schema_view_raw(request):
+    generator = schemas.SchemaGenerator(title='Billstation API RAW')
+    return response.Response(generator.get_schema(request=request))
 
 # Serializers define the API representation.
 class DudeSerializer(serializers.HyperlinkedModelSerializer):
@@ -14,10 +28,12 @@ class DudeSerializer(serializers.HyperlinkedModelSerializer):
         model = Dude
         fields = ('url', 'name', 'still_here', 'created')
 
+
 class BillSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Bill
-        fields = ('url', 'description', 'date',  'amount', 'owner', 'affected_dudes', 'created')
+        fields = ('url', 'description', 'date', 'amount', 'owner', 'affected_dudes', 'created')
+
 
 class PaymentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -30,9 +46,11 @@ class DudeViewSet(viewsets.ModelViewSet):
     queryset = Dude.objects.filter()
     serializer_class = DudeSerializer
 
+
 class BillViewSet(viewsets.ModelViewSet):
     queryset = Bill.objects.all()
     serializer_class = BillSerializer
+
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
