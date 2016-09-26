797e501ab0d80a87ca1dba72d81db679daed3b7f
Jonas Alder <jonas.alder@gmail.com>
2016-09-27 00:29:27 +0200

    - added backend logic - added tests - added rounding logic

--- station/models.py
+++ station/models.py
@@ -1,6 +1,13 @@
+from decimal import Decimal, getcontext, ROUND_HALF_UP, ROUND_HALF_DOWN
+from decimal import *
 from django.db import models
+from django.db.models import Sum
 from django.utils import timezone
 from datetime import date
+
+# Default precision of two decimal places
+getcontext().rounding = ROUND_HALF_UP
+TWOPLACES = Decimal(10) ** -2
 
 
 class Dude(models.Model):
@@ -25,6 +32,50 @@ class Bill(models.Model):
     owner = models.ForeignKey('Dude', related_name='my_bills')
     affected_dudes = models.ManyToManyField('Dude', related_name='bills_to_pay')
     created = models.DateTimeField(auto_now_add=True)
+
+    def total_remaining(self):
+        payments = self.payment_set.all().aggregate(Sum('amount'))
+        already_paid = payments['amount__sum'] if payments['amount__sum'] else 0
+        result = (Decimal(self.amount) - already_paid).quantize(TWOPLACES)
+        if result < Decimal(0):
+            return Decimal(0)
+        else:
+            return result
+
+    def share(self, dude):
+        if dude not in self.affected_dudes.all():
+            return Decimal(0)
+
+        rounding_before = getcontext().rounding
+
+        # TODO is this really needed?
+        if dude == self.owner:
+            getcontext().rounding = ROUND_HALF_UP
+        else:
+            getcontext().rounding = ROUND_HALF_DOWN
+        result = (Decimal(self.amount) / self.affected_dudes.count()).quantize(TWOPLACES)
+        getcontext().rounding = rounding_before
+        return result
+
+    def remaining(self, dude):
+        if dude not in self.affected_dudes.all():
+            return Decimal(0)
+
+        payments = self.payment_set.filter(by=dude).aggregate(Sum('amount'))
+        already_paid = payments['amount__sum'] if payments['amount__sum'] else 0
+        result = (self.share(dude) - already_paid).quantize(TWOPLACES)
+        if result < Decimal(0):
+            return Decimal(0)
+        else:
+            return result
+
+    def is_paid(self):
+        return self.total_remaining() <= Decimal(0)
+
+    def save(self, **kwargs):
+        super(Bill, self).save(**kwargs)
+        if self.owner in self.affected_dudes.all():
+            Payment.objects.create(bill=self, by=self.owner, amount=self.share(self.owner))
 
     def __str__(self):
         return '{0} - {1}'.format(self.amount, self.description[:10])
--- station/tests.py
+++ station/tests.py
@@ -1,3 +1,56 @@
+from decimal import Decimal
 from django.test import TestCase
 
 # Create your tests here.
+from station.models import Dude, Bill, Payment, TWOPLACES
+
+
+class BillAndPaymentTests(TestCase):
+    def setUp(self):
+        self.dude_one = Dude.objects.create(name="Test Dude")
+        self.dude_two = Dude.objects.create(name="Happo One")
+        self.dude_three = Dude.objects.create(name="Another Guy")
+        self.bill_even = Bill.objects.create(description="Migros", amount=12.22, owner=self.dude_one)
+        self.bill_even.affected_dudes = [self.dude_one, self.dude_two]
+        self.bill_even.save()
+
+        self.bill_odd = Bill.objects.create(description="Coop", amount=27.20, owner=self.dude_two, )
+        self.bill_odd.affected_dudes = [self.dude_one, self.dude_two, self.dude_three]
+        self.bill_odd.save()
+
+    def test_bill_basic_split(self):
+        self.assertEqual(self.bill_even.total_remaining(), Decimal(self.bill_even.amount / 2).quantize(TWOPLACES))
+        self.assertEqual(self.bill_even.remaining(self.dude_one), Decimal(0))
+        self.assertEqual(self.bill_even.remaining(self.dude_two),
+                         Decimal(self.bill_even.amount / 2).quantize(TWOPLACES))
+        self.assertFalse(self.bill_even.is_paid())
+
+        Payment.objects.create(bill=self.bill_even, by=self.dude_two, amount=self.bill_even.remaining(self.dude_two))
+
+        self.assertEqual(self.bill_even.total_remaining(), Decimal(0))
+        self.assertEqual(self.bill_even.remaining(self.dude_one), Decimal(0))
+        self.assertEqual(self.bill_even.remaining(self.dude_two), Decimal(0))
+        self.assertTrue(self.bill_even.is_paid())
+
+    def test_bill_three_way_split(self):
+        self.assertEqual(self.bill_odd.total_remaining(),
+                         (Decimal(self.bill_odd.amount) - Decimal(self.bill_odd.amount / 3)).quantize(TWOPLACES))
+        self.assertEqual(self.bill_odd.remaining(self.dude_two), Decimal(0))
+        self.assertEqual(self.bill_odd.remaining(self.dude_one), Decimal(self.bill_odd.amount / 3).quantize(TWOPLACES))
+        self.assertEqual(self.bill_odd.remaining(self.dude_three),
+                         Decimal(self.bill_odd.amount / 3).quantize(TWOPLACES))
+        self.assertFalse(self.bill_odd.is_paid())
+
+        Payment.objects.create(bill=self.bill_odd, by=self.dude_one, amount=self.bill_odd.remaining(self.dude_one))
+        self.assertAlmostEqual(self.bill_odd.total_remaining(), Decimal(self.bill_odd.amount / 3).quantize(TWOPLACES),
+                               places=1)
+        self.assertEqual(self.bill_odd.remaining(self.dude_one), Decimal(0))
+        self.assertEqual(self.bill_odd.remaining(self.dude_two), Decimal(0))
+        self.assertFalse(self.bill_odd.is_paid())
+
+        Payment.objects.create(bill=self.bill_odd, by=self.dude_three, amount=self.bill_odd.remaining(self.dude_three))
+        self.assertAlmostEqual(self.bill_odd.total_remaining(), Decimal(0), places=1)
+        self.assertEqual(self.bill_odd.remaining(self.dude_one), Decimal(0))
+        self.assertEqual(self.bill_odd.remaining(self.dude_two), Decimal(0))
+        self.assertEqual(self.bill_odd.remaining(self.dude_three), Decimal(0))
+        self.assertTrue(self.bill_odd.is_paid())
