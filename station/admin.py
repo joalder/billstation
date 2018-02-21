8ca6d1cc1b2944008919ba2c1a000014e414311b
Jonas Alder <jonas.alder@gmail.com>
2018-02-21 22:10:26 +0100

    extend bill admin

--- station/admin.py
+++ station/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+
+class PaymentInline(admin.TabularInline):
+    model = Payment
+
+
+class BillAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('description', 'owner', 'amount', 'created', 'is_paid')
+    inlines = [
+        PaymentInline,
+    ]
+
+
 admin.site.register(Dude)
-admin.site.register(Bill)
+admin.site.register(Bill, BillAdmin)
 admin.site.register(Payment)
